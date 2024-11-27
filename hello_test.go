b82eab622cf61d9a7ee8e1d79e352c838d8d3f11
Jace <contactjacenair@gmail.com>
2024-11-27 08:50:52 +0000

    Final refactoring to greetings function in the main package

--- hello_test.go
+++ hello_test.go
@@ -3,13 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jace")
+		got := Hello("Jace", "")
 		want := "Hello, Jace"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
