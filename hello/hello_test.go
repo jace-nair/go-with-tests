cf19b64da394567cab699d5cc44fb287f12abedc
Jace <contactjacenair@gmail.com>
2024-11-30 07:25:42 +0000

    Refactor code: Bind functions to local variables. Add type-safety

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,26 +3,40 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
+
+	checkGotWant := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
+		checkGotWant(t, got, want)
+		//assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Jace", "")
 		want := "Hello, Jace"
-		assertCorrectMessage(t, got, want)
+		checkGotWant(t, got, want)
+		//assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
+		checkGotWant(t, got, want)
+		//assertCorrectMessage(t, got, want)
 	})
 }
 
+// Used the below function as a local variable in the above function
+/*
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+*/
--- slices/sum_test.go
+++ slices/sum_test.go
@@ -36,7 +36,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2, 3}, []int{0, 9, 1}, []int{1, 1, 1})
 	want := []int{6, 10, 3}
 
-	checkSums(t, got, want)
+	checkGotWant(t, got, want)
 }
 
 func TestSumAllTails(t *testing.T) {
@@ -45,19 +45,19 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 1}, []int{1, 1, 1})
 		want := []int{5, 10, 2}
 
-		checkSums(t, got, want)
+		checkGotWant(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{0, 9, 1}, []int{1, 1, 1})
 		want := []int{0, 10, 2}
 
-		checkSums(t, got, want)
+		checkGotWant(t, got, want)
 	})
 
 }
 
-func checkSums(t testing.TB, got, want []int) {
+func checkGotWant(t testing.TB, got, want []int) {
 	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
