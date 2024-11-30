6e66e51ef88ef459c1ada7cccaac2f5b949ac0aa
Jace <contactjacenair@gmail.com>
2024-11-30 21:19:02 +0000

    Add structs and interfaces

---
+++ structs/calculate_test_v2.go
@@ -0,0 +1,31 @@
+package structs
+
+import "testing"
+
+// Another way is to use table driven tests.
+// Table driven tests are useful whey you want to build a list of test cases that can be tested in the same manner.
+
+func TestAreaV2(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
+}
